dd1b44869bc15be59eab42988b3d2332acd829e8
heonoo <zkfktm1525@naver.com>
2022-05-06 15:13:46 +0900

    modify loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -41,7 +41,7 @@ class YoloLoss(nn.Module):
         # Note that bestbox will be indices of 0, 1 for which bbox was best
 
         iou_maxes, bestbox = torch.max(ious, dim=0)
-        exists_box = target[..., 20].unsqueeze(3) # in paper this is Iobj_i
+        exists_box = target[..., self.C].unsqueeze(3) # in paper this is Iobj_i
 
         # ======================== #
         #   FOR BOX COORDINATES    #
@@ -49,15 +49,15 @@ class YoloLoss(nn.Module):
 
         # Set boxes with no object in them to 0. We only take out one of the
         # two predictions, which is the one with highest Iou calculated previously.
-        coord_i = self.C+1 + bestbox*5
+
         box_predictions = exists_box * (
             (
-                bestbox * predictions[..., 26:30]
-                + (1 - bestbox) * predictions[..., 21:25]
+                bestbox * predictions[..., self.C+5+1:self.C+10]
+                + (1 - bestbox) * predictions[..., self.C+1:self.C+5]
             )
         )
 
-        box_targets = exists_box * target[..., 21:25]
+        box_targets = exists_box * target[..., self.C+1:self.C+5]
 
         # Take sqrt of width, height of boxes to ensure that
         box_predictions[..., 2:4] = torch.sign(box_predictions[..., 2:4]) *\
@@ -69,4 +69,49 @@ class YoloLoss(nn.Module):
             torch.flatten(box_targets, end_dim=-2),
         )
 
-        
+        # ==================== #
+        #   FOR OBJECT LOSS    #
+        # ==================== #
+
+        # pred_box is the confidence score for the bbox with hightest IoU
+        pred_box = (
+            bestbox * predictions[..., self.C+5:self.C+5+1] +\
+                                       (1 - bestbox) * predictions[..., self.C:self.C+1]
+        )
+
+        object_loss = self.mse(
+            torch.flatten(exists_box * pred_box),
+            torch.flatten(exists_box * target[..., self.C:self.C+1])
+        )
+
+        # ======================= #
+        #   FOR NO OBJECT LOSS    #
+        # ======================= #
+
+        no_object_loss = self.mse(
+            torch.flatten((1 - exists_box) * predictions[..., self.C:self.C+1], start_dim=1),
+            torch.flatten((1 - exists_box) * target[..., self.C:self.C+1], start_dim=1)
+        )
+
+        no_object_loss += self.mse(
+            torch.flatten((1 - exists_box) * predictions[..., self.C+5:self.C+5+1], start_dim=1),
+            torch.flatten((1 - exists_box) * target[..., self.C:self.C+1], start_dim=1)
+        )
+
+        # ================== #
+        #   FOR CLASS LOSS   #
+        # ================== #
+
+        class_loss = self.mse(
+            torch.flatten(exists_box * predictions[... :self.C], end_dim=-2,),
+            torch.flatten(exists_box * target[..., :20], end_dim=-2,)
+        )
+
+        loss = (
+            self.lambda_coord * box_loss # first two rows in paper
+            + object_loss # third row in paper
+            + self.lambda_noobj * no_object_loss # forth row
+            + class_loss # fifth row
+        )
+
+        return loss
