a9cd50a62fa13d28c70a7f86030d8416715b1b35
heonoo <zkfktm1525@naver.com>
2022-08-11 16:34:05 +0900

    add model dir

---
+++ darknet.py
@@ -0,0 +1,210 @@
+from __future__ import division
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+from util import *
+
+
+def parse_cfg(cfgfile: str):
+    """
+    Args:
+        cfgfile (str): path to cfgfile
+    """
+
+    file = open(cfgfile, 'r')
+
+    lines = file.read().split('\n')
+    lines = [x for x in lines if len(x) > 0] # 빈 lines 삭제
+    lines = [x for x in lines if x[0] != '#'] # 주석 삭제
+    lines = [x.rstrip().lstrip() for x in lines] # 공백 제거
+
+    block = {}
+    blocks = []
+
+    for line in lines:
+        if line[0] == '[':
+            if len(block) != 0:
+                blocks.append(block)
+                block = {}
+            block['type'] = line[1:-1].rstrip()
+        else:
+            key, value = line.split('=')
+            block[key.rstrip()] = value.lstrip()
+    else:
+        blocks.append(block)
+
+    file.close()
+
+    return blocks
+
+
+class EmptyLayer(nn.Module):
+    def __init__(self):
+        super(EmptyLayer, self).__init__()
+
+
+class DetectionLayer(nn.Module):
+    def __init__(self, anchors):
+        super(DetectionLayer, self).__init__()
+        self.anchors = anchors
+
+
+def create_modules(blocks):
+    net_info = blocks[0]
+    module_list = nn.ModuleList()
+    prev_filters = 3
+    output_filters = []
+
+    for index, x in enumerate(blocks[1:]):
+        module = nn.Sequential()
+
+        if x['type'] == 'convolutional':
+            filters = int(x['filters'])
+            padding = int(x['pad'])
+            kernel_size = int(x['size'])
+            stride = int(x['stride'])
+
+            if padding:
+                pad = (kernel_size - 1) // 2
+            else:
+                pad = 0
+            
+            try:
+                batch_normalize = int(x['batch_normalize'])
+                bias = False
+            except:
+                batch_normalize = 0
+                bias = True
+
+
+            conv = nn.Conv2d(prev_filters, 
+                             filters, 
+                             kernel_size,
+                             stride,
+                             pad,
+                             bias=bias
+                            )
+            module.add_module(f'conv_{index}', conv)
+
+            if batch_normalize:
+                bn = nn.BatchNorm2d(filters)
+                module.add_module(f'batch_norm_{index}', bn)
+
+            activation = x['activation']
+            if activation == 'leaky':
+                activn = nn.LeakyReLU(0.1, inplace=True)
+                module.add_module(f'leaky_{index}', activn)
+
+        elif x['type'] == 'upsample':
+            stride = int(x['stride'])
+            upsample = nn.Upsample(scale_factor = 2)
+            module.add_module(f'upsample_{index}', upsample)
+
+        elif x['type'] == 'route':
+            x['layers'] = x['layers'].split(',')
+            start = int(x['layers'][0])
+            try:
+                end = int(x['layers'][1])
+            except:
+                end = 0
+            
+            if start > 0:
+                start = start - index
+            if end > 0:
+                end = end - index
+            route = EmptyLayer()
+            module.add_module(f'rout_{index}', route)
+            if end < 0:
+                filters = output_filters[index+start] + output_filters[index+end]
+            else:
+                filters = output_filters[index+start]
+
+        elif x['type'] == 'shortcut':
+            shortcut = EmptyLayer()
+            module.add_module(f"shortcut_{index}", shortcut)
+
+        elif x['type'] == 'yolo':
+            mask = x['mask'].split(',')
+            mask = [int(x) for x in mask]
+
+            anchors = x['anchors'].split(',')
+            anchors = [int(a) for a in anchors]
+            anchors = [(anchors[i], anchors[i+1]) for i in range(0, len(anchors), 2)]
+            anchors = [anchors[i] for i in mask]
+
+            detection = DetectionLayer(anchors)
+            module.add_module(f'detection_{index}', detection)
+        
+        module_list.append(module)
+        prev_filters = filters
+        output_filters.append(filters)
+
+    return (net_info, module_list)
+
+
+class Darknet(nn.Module):
+    def __init__(self, cfgfile):
+        super(Darknet, self).__init__()
+        self.blocks = parse_cfg(cfgfile)
+        self.net_info, self.module_list = create_modules(self.blocks)
+    
+    def forward(self, x, CUDA):
+        modules = self.blocks[1:]
+        outputs = {}
+
+        write = 0
+        for i, module in enumerate(modules):
+            module_type = (module['type'])
+
+            if module_type == 'convolutional' or module_type == 'upsample':
+                x = self.module_list[i](x)
+
+            elif module_type == "route":
+                layers = module["layers"]
+                layers = [int(a) for a in layers]
+
+                if (layers[0] > 0):
+                    layers[0] = layers[0] - i
+                
+                if len(layers) == 1:
+                    x = outputs[i + (layers[0])]
+                
+                else:
+                    if (layers[1]) > 0:
+                        layers[1] = layers[1] - i
+                    
+                    map1 = outputs[i + layers[0]]
+                    map2 = outputs[i + layers[1]]
+                    x = torch.cat((map1, map2), 1)
+            
+            elif module_type == "shortcut":
+                from_ = int(module["from"])
+                x = outputs[i-1] + outputs[i+from_]
+            
+            elif module_type == 'yolo':
+                anchors = self.module_list[i][0].anchors
+
+                inp_dim = int(self.net_info["height"])
+                
+                num_classes = int(module["classes"])
+
+                x = x.data
+                x = predict_transform(x, inp_dim, anchors, num_classes, CUDA)
+                if not write:
+                    detections = x
+                    write = 1
+                
+                else:
+                    detections = torch.cat((detections, x), 1)
+            
+            outputs[i] = x
+        
+        return detections
+
+
+if __name__ == '__main__':
+    blocks = parse_cfg('model/cfg/yolov3.cfg')
+    print(create_modules(blocks))
